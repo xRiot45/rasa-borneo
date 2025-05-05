edbf7bd0aea601c308db1f6a2ac18b15be91f54f
rumahkodingku <rumahkodingku45@gmail.com>
2025-05-05 11:18:42 +0700

    feat(model-backend): create model merchant and relation to users table

--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use App\Enums\PayoutStatusEnum;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Merchant extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $table = 'merchants';
+
+    protected $fillable = ['user_id', 'id_card_photo', 'business_address', 'business_details', 'tax_identification_number', 'bank_account_number', 'bank_account_name', 'bank_code', 'bank_name', 'bank_passbook_photo', 'payout_status', 'payout_verified_at'];
+
+    protected $dates = ['deleted_at'];
+
+    protected $casts = [
+        'payout_status' => PayoutStatusEnum::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,17 +4,21 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     protected $fillable = ['full_name', 'email', 'password', 'phone_number'];
 
     protected $hidden = ['password', 'remember_token'];
+
+    protected $dates = ['deleted_at'];
 
     protected function casts(): array
     {
@@ -22,5 +26,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected function merchant(): HasOne
+    {
+        return $this->hasOne(Merchant::class);
     }
 }
