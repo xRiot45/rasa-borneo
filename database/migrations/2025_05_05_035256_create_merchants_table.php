fa0186b21da0a395c88901b4b594187edfe726a6
rumahkodingku <rumahkodingku45@gmail.com>
2025-05-05 11:10:10 +0700

    refactor(enums-backend): move enum file location to enums folder

--- database/migrations/2025_05_05_035256_create_merchants_table.php
+++ database/migrations/2025_05_05_035256_create_merchants_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\PayoutStatusEnum;
+use App\Enums\PayoutStatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
