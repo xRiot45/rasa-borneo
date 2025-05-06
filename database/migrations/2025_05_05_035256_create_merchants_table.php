45bcd820e3c0cf4725dfeba309f74c1a9425a36f
rumahkodingku <rumahkodingku45@gmail.com>
2025-05-06 11:24:18 +0700

    docs(comment): add comment in merchant table

--- database/migrations/2025_05_05_035256_create_merchants_table.php
+++ database/migrations/2025_05_05_035256_create_merchants_table.php
@@ -10,20 +10,29 @@ return new class extends Migration {
     {
         Schema::create('merchants', function (Blueprint $table) {
             $table->id();
+            // Informasi Pribadi
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('id_card_photo');
+
+            // Informasi Bisnis
             $table->text('business_address');
             $table->text('business_details');
-            $table->string('tax_identification_number')->nullable();
+            $table->foreignId('business_category_id')->constrained()->onDelete('cascade');
+
+            // Informasi Rekening Bank
             $table->string('bank_account_number'); // Nomor rekening bank
             $table->string('bank_account_name'); // Nama pemilik rekening bank
             $table->string('bank_code'); // Kode bank
             $table->string('bank_name'); // Nama bank
             $table->string('bank_passbook_photo'); // Foto buku tabungan
+            $table->string('tax_identification_number')->nullable();
+
+            // Informasi Payout (Pengambilan Dana (Jika menerapkan fitur payout))
             $table->enum('payout_status', PayoutStatusEnum::values())->nullable();
             $table->timestamp('payout_verified_at')->nullable();
+
+            // Informasi Verifikasi
             $table->boolean('is_verified')->default(false);
-            $table->foreignId('business_category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
             $table->softDeletes();
         });
