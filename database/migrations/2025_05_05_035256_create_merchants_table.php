6673382cccac5d50ff411b760e15ca86ac9fd402
rumahkodingku <rumahkodingku45@gmail.com>
2025-05-06 19:51:14 +0700

    refactor(table): refactoring merchant table

--- database/migrations/2025_05_05_035256_create_merchants_table.php
+++ database/migrations/2025_05_05_035256_create_merchants_table.php
@@ -10,22 +10,24 @@ return new class extends Migration {
     {
         Schema::create('merchants', function (Blueprint $table) {
             $table->id();
-            // Informasi Pribadi
+            // Informasi Pribadi lainnya
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('id_card_photo');
 
             // Informasi Bisnis
+            $table->string('business_name');
+            $table->string('business_phone');
+            $table->string('business_email');
+            $table->string('postal_code');
+            $table->text('business_description');
             $table->text('business_address');
-            $table->text('business_details');
             $table->foreignId('business_category_id')->constrained()->onDelete('cascade');
 
             // Informasi Rekening Bank
-            $table->string('bank_account_number'); // Nomor rekening bank
-            $table->string('bank_account_name'); // Nama pemilik rekening bank
-            $table->string('bank_code'); // Kode bank
-            $table->string('bank_name'); // Nama bank
-            $table->string('bank_passbook_photo'); // Foto buku tabungan
-            $table->string('tax_identification_number')->nullable();
+            $table->string('bank_account_number'); // Nomor rekening
+            $table->string('bank_account_name'); // Nama pemilik rekening
+            $table->string('bank_code'); // Kode bank (contoh: 014 untuk BCA)
+            $table->string('tax_identification_number')->nullable(); // NPWP (opsional)
 
             // Informasi Payout (Pengambilan Dana (Jika menerapkan fitur payout))
             $table->enum('payout_status', PayoutStatusEnum::values())->nullable();
