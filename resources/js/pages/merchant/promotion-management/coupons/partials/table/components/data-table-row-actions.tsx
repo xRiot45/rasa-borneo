3326715471005daf63f841ef48b1b1ef1731874d
rumahkodingku <rumahkodingku45@gmail.com>
2025-05-14 22:12:23 +0700

    feat(coupon): get data coupon

--- resources/js/pages/merchant/promotion-management/coupons/partials/table/components/data-table-row-actions.tsx
+++ resources/js/pages/merchant/promotion-management/coupons/partials/table/components/data-table-row-actions.tsx
@@ -1,5 +1,23 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuShortcut, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { Coupon } from '@/models/coupon';
 import { Icon } from '@iconify/react';
 import { Link } from '@inertiajs/react';
@@ -7,6 +25,10 @@ import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 import { Row } from '@tanstack/react-table';
 
 export function DataTableRowActions({ row }: { row: Row<Coupon> }) {
+    const handleDelete = (id: number) => {
+        console.log(id);
+    };
+
     return (
         <>
             <DropdownMenu>
@@ -17,7 +39,7 @@ export function DataTableRowActions({ row }: { row: Row<Coupon> }) {
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end" className="w-[260px]">
-                    <Link href={route('merchant.coupon.edit', { id: row.original.id })} className="cursor-pointer">
+                    <Link href={route('merchant.coupon.edit', { id: row.original.id })}>
                         <DropdownMenuItem className="cursor-pointer p-3">
                             Edit Data
                             <DropdownMenuShortcut>
@@ -25,6 +47,30 @@ export function DataTableRowActions({ row }: { row: Row<Coupon> }) {
                             </DropdownMenuShortcut>
                         </DropdownMenuItem>
                     </Link>
+
+                    <DropdownMenuSeparator />
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <DropdownMenuItem className="cursor-pointer p-3 !text-red-500" onSelect={(e) => e.preventDefault()}>
+                                Hapus Data
+                                <DropdownMenuShortcut>
+                                    <Icon icon={'material-symbols:delete'} className="!text-red-500" />
+                                </DropdownMenuShortcut>
+                            </DropdownMenuItem>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>Hapus Kupon</AlertDialogTitle>
+                                <AlertDialogDescription>Apakah Kamu Yakin Ingin Menghapus Kupon ini?</AlertDialogDescription>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel className="cursor-pointer">Batal</AlertDialogCancel>
+                                <AlertDialogAction onClick={() => handleDelete(row.original.id)} className="cursor-pointer bg-red-600 transition-all">
+                                    Hapus
+                                </AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
                 </DropdownMenuContent>
             </DropdownMenu>
         </>
