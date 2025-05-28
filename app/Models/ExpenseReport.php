083d90ecfcfdc77feceb1637e23317c68f053141
rumahkodingku <rumahkodingku45@gmail.com>
2025-05-28 10:14:38 +0700

    feat(model): update model expense report

--- app/Models/ExpenseReport.php
+++ app/Models/ExpenseReport.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ExpenseReport extends Model
 {
-    //
+    protected $table = 'expense_reports';
+
+    protected $fillable = [
+        'merchant_id',
+        'report_date',
+        'description',
+        'total_expense',
+    ];
+
+    protected $casts = [
+        'report_date' => 'date',
+    ];
+
+    public function merchant(): BelongsTo
+    {
+        return $this->belongsTo(Merchant::class);
+    }
+
+    public function expenseReportItems(): HasMany
+    {
+        return $this->hasMany(ExpenseReportItem::class);
+    }
 }
--- app/Models/ExpenseReportItem.php
+++ app/Models/ExpenseReportItem.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ExpenseReportItem extends Model
 {
-    //
+    protected $table = 'expense_report_items';
+
+    protected $fillable = [
+        'expense_report_id',
+        'name',
+        'category_id',
+        'description',
+        'amount',
+    ];
+
+    public function expenseReport(): BelongsTo
+    {
+        return $this->belongsTo(ExpenseReport::class);
+    }
 }
