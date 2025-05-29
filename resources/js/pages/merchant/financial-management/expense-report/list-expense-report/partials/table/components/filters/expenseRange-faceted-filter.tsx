4ecaab9cafc26a4f2152bdbf331057237b48c5af
rumahkodingku <rumahkodingku45@gmail.com>
2025-05-29 21:25:16 +0700

    feat(expense-report): filter data expense report

--- resources/js/pages/merchant/financial-management/expense-report/list-expense-report/partials/table/components/filters/expenseRange-faceted-filter.tsx
+++ resources/js/pages/merchant/financial-management/expense-report/list-expense-report/partials/table/components/filters/expenseRange-faceted-filter.tsx
@@ -2,16 +2,17 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { formatCurrency } from '@/utils/format-currency';
 import { Icon } from '@iconify/react';
 import { Column } from '@tanstack/react-table';
 import { useEffect, useState } from 'react';
 
-interface TransactionRangeFilterProps<TData, TValue> {
+interface Props<TData, TValue> {
     column: Column<TData, TValue>;
     title?: string;
 }
 
-export function TransactionRangeFacetedFilter<TData, TValue>({ column, title = 'Tanggal' }: TransactionRangeFilterProps<TData, TValue>) {
+export function ExpenseRangeFilter<TData, TValue>({ column, title = 'Tanggal' }: Props<TData, TValue>) {
     const selectedValue = column.getFilterValue() as { min?: number; max?: number } | undefined;
     const [range, setRange] = useState<typeof selectedValue>(selectedValue || { min: undefined, max: undefined });
 
@@ -27,8 +28,8 @@ export function TransactionRangeFacetedFilter<TData, TValue>({ column, title = '
         <Popover>
             <PopoverTrigger>
                 <Button variant="outline" className="w-[350px] justify-start text-left font-normal">
-                    <Icon icon="mdi:filter-variant" className="mr-2 h-4 w-4" />
-                    {range?.min || range?.max ? `Transaksi: ${range?.min ?? '0'} - ${range?.max ?? '∞'}` : title}
+                    <Icon icon="majesticons:money-line" className="mr-2 h-4 w-4" />
+                    {range?.min || range?.max ? `Pengeluaran: ${formatCurrency(range?.min ?? 0)} - ${formatCurrency(range?.max ?? 0)}` : title}
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="z-50 mt-3 w-[350px] space-y-3">
@@ -36,7 +37,7 @@ export function TransactionRangeFacetedFilter<TData, TValue>({ column, title = '
                     <Label className="text-sm font-medium">Min</Label>
                     <Input
                         type="number"
-                        placeholder="Minimal transaksi"
+                        placeholder="Minimal Pengeluaran"
                         value={range?.min ?? ''}
                         onChange={(e) => {
                             const value = e.target.value;
@@ -51,7 +52,7 @@ export function TransactionRangeFacetedFilter<TData, TValue>({ column, title = '
                     <label className="text-sm font-medium">Max</label>
                     <Input
                         type="number"
-                        placeholder="Maksimal transaksi"
+                        placeholder="Maksimal Pengeluaran"
                         value={range?.max ?? ''}
                         onChange={(e) => {
                             const value = e.target.value;
