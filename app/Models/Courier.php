fb200abfab28941c1a862f9ad580d184df4d8da6
rumahkodingku <rumahkodingku45@gmail.com>
2025-05-31 10:50:37 +0700

    feat(courier): setup model courier

--- app/Models/Courier.php
+++ app/Models/Courier.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use App\Enums\GenderEnum;
+use App\Enums\VehicleTypeEnum;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Courier extends Model
 {
-    //
+    protected $table = 'couriers';
+
+    protected $fillable = [
+        'user_id',
+        'vehicle_type',
+        'national_id',
+        'id_card_photo',
+        'age',
+        'birthplace',
+        'birthdate',
+        'profile_image',
+        'gender',
+        'driving_license_photo',
+        'is_online',
+        'is_verified',
+    ];
+
+    protected $casts = [
+        'vehicle_type' => VehicleTypeEnum::class,
+        'gender' => GenderEnum::class,
+        'birthdate' => 'date:Y-m-d',
+        'is_online' => 'boolean',
+        'is_verified' => 'boolean',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
