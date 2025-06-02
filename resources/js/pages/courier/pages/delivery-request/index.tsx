b7e98f9f5c776fe3d9ac46876058b592f2706777
rumahkodingku <rumahkodingku45@gmail.com>
2025-06-02 21:50:59 +0700

    feat(courier): add conditional rendering

--- resources/js/pages/courier/pages/delivery-request/index.tsx
+++ resources/js/pages/courier/pages/delivery-request/index.tsx
@@ -1,3 +1,4 @@
+import EmptyImage from '@/assets/errors/empty.svg';
 import DefaultPhotoProfile from '@/assets/images/default-image.png';
 import OrderStatusBadge from '@/components/order-status-badge';
 import { Badge } from '@/components/ui/badge';
@@ -120,100 +121,116 @@ export default function OrderRequestPage({ orders }: Props) {
                         </Button>
                     </div>
 
-                    <div className="mt-8">
-                        {orders.map((order) => (
-                            <Card key={order.id} className="mb-4 py-6 shadow-none">
-                                <CardHeader className="flex flex-col">
-                                    <div className="flex flex-col gap-4 sm:flex-row sm:items-start sm:justify-between">
-                                        <div className="flex items-start gap-4">
-                                            <img
-                                                src={`${order?.merchant?.store_profile?.logo_photo}` || DefaultPhotoProfile}
-                                                alt="Logo Merchant"
-                                                className="h-20 w-20 rounded-lg border object-cover"
-                                            />
-                                            <div className="flex flex-col items-start space-y-1">
-                                                <h3 className="text-md font-bold">{order.merchant.business_name}</h3>
-                                                <p className="text-muted-foreground text-sm">{order?.merchant?.business_category?.name}</p>
-                                                <p className="text-muted-foreground text-sm">{order?.merchant?.business_address}</p>{' '}
-                                                {/* <- Tambahkan ini */}
-                                                <Badge className="mt-1 rounded-sm">{order.transaction_items.length} Pesanan</Badge>
+                    {orders?.length > 0 ? (
+                        <div className="mt-8">
+                            {orders.map((order) => (
+                                <Card key={order.id} className="mb-4 py-6 shadow-none">
+                                    <CardHeader className="flex flex-col">
+                                        <div className="flex flex-col gap-4 sm:flex-row sm:items-start sm:justify-between">
+                                            <div className="flex items-start gap-4">
+                                                <img
+                                                    src={`${order?.merchant?.store_profile?.logo_photo}` || DefaultPhotoProfile}
+                                                    alt="Logo Merchant"
+                                                    className="h-20 w-20 rounded-lg border object-cover"
+                                                />
+                                                <div className="flex flex-col items-start space-y-1">
+                                                    <h3 className="text-md font-bold">{order.merchant.business_name}</h3>
+                                                    <p className="text-muted-foreground text-sm">{order?.merchant?.business_category?.name}</p>
+                                                    <p className="text-muted-foreground text-sm">{order?.merchant?.business_address}</p>{' '}
+                                                    {/* <- Tambahkan ini */}
+                                                    <Badge className="mt-1 rounded-sm">{order.transaction_items.length} Pesanan</Badge>
+                                                </div>
+                                            </div>
+
+                                            <div className="flex flex-col gap-2 space-y-1 sm:mt-2 sm:flex-col sm:items-end">
+                                                <p className="mt-1 text-sm font-medium">{formatDate(order.checked_out_at ?? '')}</p>
+                                                <OrderStatusBadge status={order.latest_order_status.status as OrderStatusEnum} />
+                                            </div>
+                                        </div>
+                                    </CardHeader>
+
+                                    <CardContent className="text-muted-foreground space-y-6 text-sm">
+                                        <div className="grid grid-cols-1 gap-x-6 gap-y-3 sm:grid-cols-2">
+                                            <div>
+                                                <Label>Label Alamat</Label>
+                                                <p className="text-primary mt-1 text-base font-semibold capitalize">
+                                                    {order.recipient_address_label || '-'}
+                                                </p>
+                                            </div>
+
+                                            <div>
+                                                <Label>Penerima</Label>
+                                                <p className="text-primary mt-1 text-base font-semibold">{order.recipient_name}</p>
+                                                <p className="text-muted-foreground text-sm">{order.recipient_phone_number}</p>
+                                            </div>
+
+                                            <div className="sm:col-span-2">
+                                                <Label>Email</Label>
+                                                <p className="text-primary mt-1 text-base font-semibold">{order.recipient_email || '-'}</p>
+                                            </div>
+
+                                            <div className="sm:col-span-2">
+                                                <Label>Alamat Lengkap</Label>
+                                                <p className="text-primary mt-1 text-base font-semibold">{order.recipient_address}</p>
+                                            </div>
+
+                                            <div className="sm:col-span-2">
+                                                <Label>Catatan Pengiriman</Label>
+                                                <p className="text-primary mt-1 text-base font-semibold">{order.delivery_note || '-'}</p>
                                             </div>
                                         </div>
 
-                                        <div className="flex flex-col gap-2 space-y-1 sm:mt-2 sm:flex-col sm:items-end">
-                                            <p className="mt-1 text-sm font-medium">{formatDate(order.checked_out_at ?? '')}</p>
-                                            <OrderStatusBadge status={order.latest_order_status.status as OrderStatusEnum} />
+                                        <Separator />
+
+                                        <div className="flex flex-wrap justify-between gap-4">
+                                            <div>
+                                                <Label>Kode Transaksi</Label>
+                                                <p className="text-primary mt-1 text-base font-semibold">{order.transaction_code}</p>
+                                            </div>
+
+                                            <div>
+                                                <Label>Total Pembayaran</Label>
+                                                <p className="text-primary mt-1 text-end text-base font-semibold">
+                                                    {formatCurrency(order?.final_total)}
+                                                </p>
+                                            </div>
                                         </div>
-                                    </div>
-                                </CardHeader>
+                                    </CardContent>
 
-                                <CardContent className="text-muted-foreground space-y-6 text-sm">
-                                    <div className="grid grid-cols-1 gap-x-6 gap-y-3 sm:grid-cols-2">
-                                        <div>
-                                            <Label>Label Alamat</Label>
-                                            <p className="text-primary mt-1 text-base font-semibold capitalize">
-                                                {order.recipient_address_label || '-'}
-                                            </p>
-                                        </div>
-
-                                        <div>
-                                            <Label>Penerima</Label>
-                                            <p className="text-primary mt-1 text-base font-semibold">{order.recipient_name}</p>
-                                            <p className="text-muted-foreground text-sm">{order.recipient_phone_number}</p>
-                                        </div>
-
-                                        <div className="sm:col-span-2">
-                                            <Label>Email</Label>
-                                            <p className="text-primary mt-1 text-base font-semibold">{order.recipient_email || '-'}</p>
-                                        </div>
-
-                                        <div className="sm:col-span-2">
-                                            <Label>Alamat Lengkap</Label>
-                                            <p className="text-primary mt-1 text-base font-semibold">{order.recipient_address}</p>
-                                        </div>
-
-                                        <div className="sm:col-span-2">
-                                            <Label>Catatan Pengiriman</Label>
-                                            <p className="text-primary mt-1 text-base font-semibold">{order.delivery_note || '-'}</p>
-                                        </div>
-                                    </div>
-
-                                    <Separator />
-
-                                    <div className="flex flex-wrap justify-between gap-4">
-                                        <div>
-                                            <Label>Kode Transaksi</Label>
-                                            <p className="text-primary mt-1 text-base font-semibold">{order.transaction_code}</p>
-                                        </div>
-
-                                        <div>
-                                            <Label>Total Pembayaran</Label>
-                                            <p className="text-primary mt-1 text-end text-base font-semibold">{formatCurrency(order?.final_total)}</p>
-                                        </div>
-                                    </div>
-                                </CardContent>
-
-                                <CardFooter className="flex gap-3 pt-2">
-                                    <Button
-                                        variant="destructive"
-                                        className="flex-1 cursor-pointer py-6 text-sm hover:bg-red-700"
-                                        onClick={() => handleConfirmRejectedOrder(order?.id)}
-                                    >
-                                        Tolak Pesanan
-                                        <Icon icon={'material-symbols:cancel'} className="text-background ml-2" />
-                                    </Button>
-                                    <Button
-                                        variant="default"
-                                        className="flex-1 cursor-pointer py-6 text-sm"
-                                        onClick={() => handleConfirmAcceptedOrder(order?.id)}
-                                    >
-                                        Terima Pesanan
-                                        <Icon icon={'material-symbols:check'} className="text-background ml-2" />
-                                    </Button>
-                                </CardFooter>
-                            </Card>
-                        ))}
-                    </div>
+                                    <CardFooter className="flex gap-3 pt-2">
+                                        <Button
+                                            variant="destructive"
+                                            className="flex-1 cursor-pointer py-6 text-sm hover:bg-red-700"
+                                            onClick={() => handleConfirmRejectedOrder(order?.id)}
+                                        >
+                                            Tolak Pesanan
+                                            <Icon icon={'material-symbols:cancel'} className="text-background ml-2" />
+                                        </Button>
+                                        <Button
+                                            variant="default"
+                                            className="flex-1 cursor-pointer py-6 text-sm"
+                                            onClick={() => handleConfirmAcceptedOrder(order?.id)}
+                                        >
+                                            Terima Pesanan
+                                            <Icon icon={'material-symbols:check'} className="text-background ml-2" />
+                                        </Button>
+                                    </CardFooter>
+                                </Card>
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="flex flex-col">
+                            <div className="flex grow items-center px-6 xl:px-10">
+                                <div className="mx-auto text-center">
+                                    <img src={EmptyImage} alt="Error" className="mx-auto mb-8 w-full max-w-lg lg:mb-12 2xl:mb-16" />
+                                    <h1 className="text-xl font-bold">Tidak Ada Permintaan Pengantaran</h1>
+                                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                                        Tidak ada permintaan pengantaran saat ini. Silakan coba lagi nanti.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    )}
 
                     {/* Dialog Accepted Order */}
                     <Dialog open={showDialogAcceptedOrder} onOpenChange={setShowDialogAcceptedOrder}>
