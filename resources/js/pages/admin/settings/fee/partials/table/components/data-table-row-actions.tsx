84b2967a87bdbaf3e8b3b0d68b625b78d8a482d3
rumahkodingku <rumahkodingku45@gmail.com>
2025-06-18 10:30:44 +0700

    refactor: refactoring code

--- resources/js/pages/admin/settings/fee/partials/table/components/data-table-row-actions.tsx
+++ resources/js/pages/admin/settings/fee/partials/table/components/data-table-row-actions.tsx
@@ -70,7 +70,7 @@ export function DataTableRowActions({ row }: { row: Row<FeeItem> }) {
                     </DialogHeader>
                     <div className="grid gap-2 py-4">
                         <Label className="text-sm font-medium capitalize">{row.original.type}</Label>
-                        <Input type="number" value={amount} onChange={(e) => setAmount(parseInt(e.target.value, 10) || 0)} />
+                        <Input type="number" value={amount} onChange={(e) => setAmount(parseInt(e.target.value, 10) || 0)} className="py-6 rounded-lg" />
                     </div>
                     <DialogFooter>
                         <Button variant="outline" onClick={() => setShowDialog(false)}>
