234e3df3c9448eac1e607c9db868539997a32cb7
rumahkodingku <rumahkodingku45@gmail.com>
2025-07-09 21:28:36 +0700

    refactor: refactoring code

--- resources/js/pages/admin/pages/users-management/admins/partials/table/components/data-table-toolbar.tsx
+++ resources/js/pages/admin/pages/users-management/admins/partials/table/components/data-table-toolbar.tsx
@@ -47,21 +47,21 @@ export function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>)
                     placeholder="Cari nama..."
                     value={(table.getColumn('full_name')?.getFilterValue() as string) ?? ''}
                     onChange={(event) => table.getColumn('full_name')?.setFilterValue(event.target.value)}
-                    className="h-8 w-full sm:w-[150px] lg:w-[250px]"
+                    className="h-10 w-full shadow-none sm:w-[150px] lg:w-[250px]"
                 />
 
                 <Input
                     placeholder="Cari email..."
                     value={(table.getColumn('email')?.getFilterValue() as string) ?? ''}
                     onChange={(event) => table.getColumn('email')?.setFilterValue(event.target.value)}
-                    className="h-8 w-full sm:w-[150px] lg:w-[250px]"
+                    className="h-10 w-full shadow-none sm:w-[150px] lg:w-[250px]"
                 />
 
                 <Input
                     placeholder="Cari nomor telepon..."
                     value={(table.getColumn('phone_number')?.getFilterValue() as string) ?? ''}
                     onChange={(event) => table.getColumn('phone_number')?.setFilterValue(event.target.value)}
-                    className="h-8 w-full sm:w-[150px] lg:w-[250px]"
+                    className="h-10 w-full shadow-none sm:w-[150px] lg:w-[250px]"
                 />
 
                 <div className="flex gap-x-2">
