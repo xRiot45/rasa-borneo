98c2815dce54017a2cc33cfe385b1a33fec4e02b
rumahkodingku <rumahkodingku45@gmail.com>
2025-08-05 21:03:35 +0700

    refactor: refactoring code

--- resources/js/pages/merchant/pages/store-management/table/partials/table/components/data-table-toolbar.tsx
+++ resources/js/pages/merchant/pages/store-management/table/partials/table/components/data-table-toolbar.tsx
@@ -10,7 +10,7 @@ export function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>)
         <div className="flex items-center justify-between">
             <div className="flex flex-1 flex-col-reverse items-start gap-y-2 sm:flex-row sm:items-center sm:space-x-2">
                 <Input
-                    placeholder="Cari nama kategori bisnis..."
+                    placeholder="Cari nomor meja..."
                     value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
                     onChange={(event) => table.getColumn('name')?.setFilterValue(event.target.value)}
                     className="h-8 w-[150px] lg:w-[250px]"
